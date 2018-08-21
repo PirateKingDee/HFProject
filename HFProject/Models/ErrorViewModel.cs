ed44cef0735e95c2b492d1ee0183270c904fb0bf
PirateKingDee <andy.liang@tallan.com>
2018-08-21 15:44:59 -0700

    first commit

---
+++ HFProject/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace HFProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
